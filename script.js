bf407434a197b6cfd3b4c795476626477abbdb5c
arthur222222222210 <arthur.santos.oliveira@escola.pr.gov.br>
2024-10-10 09:35:34 -0300

    update

--- script.js
+++ script.js
@@ -13,8 +13,23 @@ function calcularValores() {
 
     const delta = b * b - 4 * a * c;
 
-    resultado.innerHTML = `delta = ${delta}`;
+    resultado.innerHTML += `delta = ${delta}`;
+
+    if (delta > 0) {
+        resultado.innerHTML = '  <p>As raízes sâo distintas</p>';
+
+        const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        resultado.innerHTML += `x1 = ${x1.toFixed(4)} <br>`;
+        resultado.innerHTML += `x2 = ${x2.toFixed(2)} <br>`;
+    } else if (delta == 0) {
+        resultado.innerHTML = '  <p>As raízes sâo iguais</p>';
+        const x2 = -b / (2 * a);
+        resultado.innerHTML += `x1 = x2 = ${x2.toFixed(2)} <br>`;
+    } else {
+        resultado.innerHTML = '  <p>Nâo existe raízes reais</p>';
+    }
 
 }
 
-botao.addEventListener('click', calcularValores,false);
+botao.addEventListener('click', calcularValores, false);
